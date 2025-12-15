ae06fd4c76b10f5e9fa440fdf0474b66f7726809
Davidemauri00 <dadi.spyro@gmail.com>
2025-12-15 16:32:37 +0100

    ese $4: completato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,27 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function checkVocal (lettera){
+    if (lettera === "a" || lettera === "e" || lettera === "i" || lettera === "o" || lettera === "u"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
+function vocals (parola){
+    let j = 0;
+    for (i = 0;i < parola.length; i++){
+        if (checkVocal(parola.charAt(i))){
+            j++;
+        }
+    }
+    return j;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
