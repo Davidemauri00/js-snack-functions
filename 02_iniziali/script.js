5f17cec0001067300ee3e8390d2444004f014a65
Davidemauri00 <dadi.spyro@gmail.com>
2025-12-15 15:35:48 +0100

    ese 2 completato

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali (listaNomi){
+    let listaIniziali = [];
+    for (let i = 0; i < listaNomi.length; i++){
+        listaIniziali.push(listaNomi[i].charAt(0));
+    }
+    return listaIniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(iniziali(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
