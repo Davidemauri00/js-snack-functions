9c992f2f28aa2259bce213863f67640cf496563e
Davidemauri00 <dadi.spyro@gmail.com>
2025-12-15 15:38:20 +0100

    funzione iniziali ese 3 copiata da ese 2

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,7 +4,13 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali (listaNomi){
+    let listaIniziali = [];
+    for (let i = 0; i < listaNomi.length; i++){
+        listaIniziali.push(listaNomi[i].charAt(0));
+    }
+    return listaIniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
